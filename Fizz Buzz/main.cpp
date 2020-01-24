7107b5b3fbf61e178ef6d2baacdecf0226e3cb63
Roooooobin <35558127+Roooooobin@users.noreply.github.com>
2020-01-24 11:03:41 +0800

    add leetcode-412

---
+++ Fizz Buzz/main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> fizzBuzz(int n) {
+    vector<string> ans(n);
+    for(int i=1; i<=n; i++) {
+        ans[i-1] = to_string(i);
+    }
+    for(int i=2; i<n; i+=3) {
+        ans[i] = "Fizz";
+    }
+    for(int i=4; i<n; i+=5) {
+        ans[i] = "Buzz";
+    }
+    for(int i=14; i<n; i+=15) {
+        ans[i] = "FizzBuzz";
+    }
+    return ans;
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
