f128d7ef7ca9d399cc5918c5382672c1e04f546d
Roooooobin <35558127+Roooooobin@users.noreply.github.com>
2020-02-14 10:26:34 +0800

    add leetcode-962

--- Maximum Width Ramp/main.cpp
+++ Maximum Width Ramp/main.cpp
@@ -1,32 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int maxWidthRamp(vector<int>& A) {
-    int n = A.size();
-    int max_ = 0;
-    for(int i=0; i<n; i++){
-        for(int j=n-1; j>=i; j--){
-            if(A[j] >= A[i]){
-                max_ = max(max_, j-i);
-                break;
-            }
-        }
-    }
-    return max_;
-}
+//TLE
+//int maxWidthRamp(vector<int>& A) {
+//    int n = A.size();
+//    int max_ = 0;
+//    for(int i=0; i<n; i++){
+//        for(int j=n-1; j>=i; j--){
+//            if(A[j] >= A[i]){
+//                max_ = max(max_, j-i);
+//                break;
+//            }
+//        }
+//    }
+//    return max_;
+//}
+
+//TLE
+//int maxWidthRamp(vector<int>& A) {
+//    int n = A.size();
+//    int max_ = 0;
+//    for(int i=0; i<n; i++){
+//        for(int j=i; j<n; j++){
+//            if(A[j] >= A[i]) max_ = max(max_, j-i);
+//        }
+//    }
+//    return max_;
+//}
 
 int maxWidthRamp(vector<int>& A) {
     int n = A.size();
-    int max_ = 0;
+    multimap<int, int> index;
     for(int i=0; i<n; i++){
-        for(int j=i; j<n; j++){
-            if(A[j] >= A[i]) max_ = max(max_, j-i);
-        }
+        index.insert({A[i], i});
     }
-    return max_;
+    int min_ = n;
+    int ans = 0;
+    for(auto& it: index){
+        ans = max(ans, it.second-min_);
+        min_ = min(min_, it.second);
+    }
+    return ans;
 }
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<int> A = {6, 0, 8, 2, 1, 5};
+    cout << maxWidthRamp(A) << endl;
     return 0;
 }
