18ceff90771b260a37213dbc956115483be801c0
Roooooobin <35558127+Roooooobin@users.noreply.github.com>
2020-03-14 11:03:27 +0800

    add leetcode-378(binary search)

--- Kth Smallest Element in a Sorted Matrix/main.cpp
+++ Kth Smallest Element in a Sorted Matrix/main.cpp
@@ -1,22 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// brute force
+//int kthSmallest(vector<vector<int>>& matrix, int k) {
+//    int n = matrix.size();
+//    vector<int> arr;
+//    for(int i=0; i<n; i++){
+//        for(int j=0; j<n; j++){
+//            arr.push_back(matrix[i][j]);
+//        }
+//    }
+//    sort(arr.begin(), arr.end());
+//    return arr[k-1];
+//}
+
+// binary search
 int kthSmallest(vector<vector<int>>& matrix, int k) {
     int n = matrix.size();
-    vector<int> arr;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            arr.push_back(matrix[i][j]);
+    int l = matrix[0][0], r = matrix[n-1][n-1], mid;
+    while(l < r){
+        mid = (l+r) >> 1;
+        int cnt = 0, j = n-1;
+        for(int i=0; i<n; i++){
+            while(j >= 0 && matrix[i][j] > mid) j--;
+            cnt += j+1;
+        }
+        if(cnt >= k){
+            r = mid;
+        }
+        else{
+            l = mid+1;
         }
     }
-    sort(arr.begin(), arr.end());
-    return arr[k-1];
-}
-
-int kthSmallest(vector<vector<int>>& matrix, int k) {
-    int n = matrix.size();
-    vector<int> arr;
-
+    return l;
 }
 
 int main() {
